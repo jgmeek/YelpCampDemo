6fdc63c8f902399860818cc9ecd2699afbf0e433
Justin Meek <justin.meek101@gmail.com>
2018-04-21 20:56:49 +0000

    Created show route and added description tag to campground Schema

--- app.js
+++ app.js
@@ -10,11 +10,27 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
    name: String,
-   image: String
+   image: String,
+   description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
+/*Campground.create(
+   {
+      name: "Jasper",
+      image: "https://i2.wp.com/www.jaspernationalpark.com/wp-content/uploads/2010/07/JasperNationalPark.jpg",
+      description: "Beautiful Campground!"
+   },
+   function(err, campground) {
+       if(err){
+          console.log(err);
+       }
+       else {
+          console.log("New campground created: ");
+          console.log(campground);
+       }
+   });*/
 
 app.get("/", function(req, res){
    res.render("landing"); 
@@ -26,7 +42,7 @@ app.get("/campgrounds", function(req, res){
          console.log("Failed to get all campgrounds.");
          console.log(err);
       } else {
-         res.render("campgrounds", {campgrounds:allCampgrounds})
+         res.render("index", {campgrounds:allCampgrounds})
       }
    });
 });
@@ -36,8 +52,9 @@ app.post("/campgrounds", function(req, res){
    //redirect back to campgrounds page
    var name = req.body.name;
    var image = req.body.image;
+   var description = req.body.description;
    console.log(image);
-   var newCampground = {name: name, image: image};
+   var newCampground = {name: name, image: image, description: description};
    
    Campground.create(newCampground, function(err, newlyCreated){
       if(err) {
@@ -53,6 +70,20 @@ app.post("/campgrounds", function(req, res){
 app.get("/campgrounds/new", function(req, res) {
    res.render("new.ejs");
 });
+
+
+// SHOW - Shows more information about one campgronud
+app.get("/campgrounds/:id", function(req, res) {
+   //Get campground based on id
+   Campground.findById(req.params.id, function(err, foundCampground){
+      if(err){
+         console.log(err);
+      }
+      else {
+         res.render("show", {campground: foundCampground});
+      }
+   });
+})
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started"); 
