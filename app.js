1a6064e285f9525974882a6e9d8b019c4a60a443
Justin Meek <justin.meek101@gmail.com>
2018-04-24 02:04:11 +0000

    Created new comment route, and show comments on campground page

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get("/campgrounds", function(req, res){
          console.log("Failed to get all campgrounds.");
          console.log(err);
       } else {
-         res.render("index", {campgrounds:allCampgrounds})
+         res.render("campgrounds/index", {campgrounds:allCampgrounds})
       }
    });
 });
@@ -47,7 +47,7 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/new", function(req, res) {
-   res.render("new.ejs");
+   res.render("campgrounds/new");
 });
 
 
@@ -59,10 +59,43 @@ app.get("/campgrounds/:id", function(req, res) {
          console.log(err);
       }
       else {
-         res.render("show", {campground: foundCampground});
+         res.render("campgrounds/show", {campground: foundCampground});
       }
    });
 })
+
+// =================
+// COMMENTS ROUTES
+// =================
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+   Campground.findById(req.params.id, function(err, campground){
+      if(err){
+         console.log(err);
+      } else {
+         res.render("comments/new", {campground: campground});
+      }
+   })
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+   Campground.findById(req.params.id, function(err, campground) {
+       if(err) {
+          console.log(err);
+          res.redirect("/campgrounds");
+       } else {
+          Comment.create(req.body.comment, function(err, comment){
+             if(err) {
+                console.log(err);
+             } else {
+                campground.comments.push(comment);
+                campground.save();
+                res.redirect('/campgrounds/' + campground._id);
+             }
+          });
+       }
+   });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started"); 
