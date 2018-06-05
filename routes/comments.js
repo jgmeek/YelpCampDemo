be5787d41eab6e73cbe44c82a0dcca7a3bf009f4
Justin Meek <justin.meek101@gmail.com>
2018-06-05 04:10:03 +0000

    Added authorization for comment editing and deletion

--- routes/comments.js
+++ routes/comments.js
@@ -39,7 +39,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 // COMMENTS EDIT
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership,function(req, res) {
    Comment.findById(req.params.comment_id, function(err, foundComment) {
        if(err){
           res.redirect("back");
@@ -50,7 +50,7 @@ router.get("/:comment_id/edit", function(req, res) {
 });
 
 // COMMENTS UPDATE
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership,function(req, res) {
    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
       if(err){
          res.redirect("back");
@@ -61,7 +61,7 @@ router.put("/:comment_id", function(req, res) {
 });
 
 // COMMENTS DESTROY
-router.delete("/:comment_id", function(req, res){
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
    Comment.findByIdAndRemove(req.params.comment_id, function(err){
       if(err){
          res.redirect("back");
@@ -77,7 +77,25 @@ function isLoggedIn(req, res, next){
       return next();
    }
    res.redirect("/login");
-};
+}
+
+function checkCommentOwnership(req, res, next) {
+   if(req.isAuthenticated()) {
+      Comment.findById(req.params.comment_id, function(err, foundComment){
+         if(err){
+            res.redirect("back");
+         } else {
+            if(foundComment.author.id.equals(req.user._id)) {
+               next();
+            } else {
+               res.redirect("back");
+            }
+         }
+      });
+   } else {
+      res.redirect("back");
+   }
+}
 
 
 module.exports = router;
